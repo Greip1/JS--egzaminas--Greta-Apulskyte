9460a6c3e8c336d20a868f72ebab7e6c091bb29c
Greta <gretaapul@gmail..com>
2022-04-04 16:50:12 +0300

    4uzd css added

--- 30 paskaita (egzaminas)/Task 4/script.js
+++ 30 paskaita (egzaminas)/Task 4/script.js
@@ -15,35 +15,25 @@ const baseUrl = "cars.json";
 async function getData() {
   const resp = await fetch(baseUrl);
   const dataInJs = await resp.json();
-  console.log(`exp dataInJs===`, dataInJs);
   const brand = dataInJs.map((ob) => ob.brand);
-  console.log(brand);
   const model = dataInJs.map((ob) => ob.models);
-  console.log(model);
-
-  createExpPost(dataInJs, divEl);
+  createPost(dataInJs, divEl);
 }
 getData();
 
-function createExpPost(dataArr, destination) {
+function createPost(dataArr, destination) {
   destination.innerHTML = "";
   dataArr.forEach((ob) => {
-    // console.log(`cia yra ob==`, ob);
-    destination.append(createExpBlock(ob));
+    destination.append(createCard(ob));
   });
 }
 
-function createExpBlock(ob) {
+function createCard(ob) {
   const newDiv = document.createElement("div");
   newDiv.setAttribute("class", "brand-container");
   newDiv.innerHTML = `
-  
-            <h3 class="brand">${ob.brand}</h3>
-
-            <p class="model"> ${ob.models} </p>
-          
-
+    <h3 class="brand">${ob.brand}</h3>
+    <p class="model"> ${ob.models} </p>
   `;
-  console.log(`cia div el`, newDiv);
   return newDiv;
 }
